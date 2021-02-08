87f41fd35a99c28b6b6b1735ac8834548db12d92
kamrank <kamran.matloob@gmail.com>
2021-02-09 01:51:26 +1300

    change app text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Kamran
+          I was changed on the Feature branch
         </a>
       </header>
     </div>
